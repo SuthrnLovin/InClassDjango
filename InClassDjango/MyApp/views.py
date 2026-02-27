4683bf1f5115939589ef15f397e54290c8a16193
Jalen Chambers <4061219@schoolsnet.act.edu.au>
2026-02-27 11:59:41 +1100

    EOD

--- InClassDjango/MyApp/views.py
+++ InClassDjango/MyApp/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import teacher
 
 # Create your views here.
 def index (request):
-    now = datetime.now()
+    teach = teacher.objects.all()
 
-    return render(
-        request, "MyApp/index.html"
-                  {
-                      'content' : html_content += "Hello Django! on " + now.strftime("%A, %d %B, %Y at %X")
-                  }
-                  
-                  )
 
-    html_content = "<html><head><title><Hello Django>/title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-
-    return HttpResponse("Hello Django")
+    return render(request, "MyApp/index.html", {'content': teach})
+ 
