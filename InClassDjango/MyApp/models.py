c536e70ee9acc297004e1b52bd468c83bfceb4bc
Jalen Chambers <4061219@schoolsnet.act.edu.au>
2026-03-02 10:21:43 +1100

    EOD

--- InClassDjango/MyApp/models.py
+++ InClassDjango/MyApp/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class teacher (models.Model):
     Name = models.CharField(max_length=25)
     Area = models.CharField(max_length=30)
+    Course = models.CharField(max_length=20)
