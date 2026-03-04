cb551fd7972ee95135d3efc72cf921877cef54e2
Jalen Chambers <4061219@schoolsnet.act.edu.au>
2026-03-04 14:59:12 +1100

    Eod

--- InClassDjango/MyApp/models.py
+++ InClassDjango/MyApp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# BSS information
 class teacher (models.Model):
     Name = models.CharField(max_length=25)
     Area = models.CharField(max_length=30)
     Course = models.CharField(max_length=20)
+    
+# manual information
