75f621ff768daf7626d6a86a3856e96406a5fe5f
Jalen Chambers <4061219@schoolsnet.act.edu.au>
2026-03-17 18:11:52 +1100

    NEARLY there

--- InClassDjango/MyApp/views.py
+++ InClassDjango/MyApp/views.py
@@ -10,10 +10,19 @@ def index (request):
     context = {}
     teach = teacher.objects.all()
     form = teacherform()
+    if request.method == "POST":
+        if 'save' in request.POST:
+            form = teacherform(request.POST)
+            form.save()
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            teach = form.objects.get(id=pk)
+            teach.delete()
+
+
     context['form'] = form
     context['teach'] = teach
-   
-
+    
     return render(request, "MyApp/index.html", context)
 
  
